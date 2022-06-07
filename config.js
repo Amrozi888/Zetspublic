6891d6e32bd21ee24e19d20c70385d9d1378dda6
Amrozi888 <97621090+Amrozi888@users.noreply.github.com>
2022-06-07 12:07:49 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,7 +40,7 @@ global.limitawal = {
 }
 global.sc = fs.readFileSync('./media/sc.jpg')
 global.tq = fs.readFileSync('./media/tq.jpg')
-global.menu = fs.readFileSync('./media/IMG-20220605-WA0030.jpg')
+global.menu = fs.readFileSync('./media/menu.jpg')
 global.allmenu = fs.readFileSync('./media/IMG-20220605-WA0033.jpg')
 global.thumb = fs.readFileSync('./media/IMG-20220604-WA0033.jpg')
 
